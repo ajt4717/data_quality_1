fd9d6e177db5422200d6b80379f09f8037d9fb61
ajt4717 <ajt4717@gmail.com>
2025-04-19 16:02:23 +0530

    adding page database_mysql

---
+++ pages/database_mysql.py
@@ -0,0 +1,58 @@
+import streamlit as st
+import pandas as pd
+
+st.header("Database QC")
+
+import mysql.connector
+
+# connecting to the database 
+mydb = mysql.connector.connect(
+  host="bhhdeiho5kfkljyqx1ae-mysql.services.clever-cloud.com",
+  user="uqnhvavizgrbe5jl",
+  password="uT5V89vG6K4BTHigg7vT",
+  port="3306",
+  database="bhhdeiho5kfkljyqx1ae",
+  use_pure=True
+)
+
+#test connection
+print("**********   **********")
+print(mydb)
+
+# preparing a cursor object 
+cursor = mydb.cursor()
+
+print("********** single query **********")
+#querying
+query = """
+show databases;
+"""
+
+cursor.execute(query)
+for x in cursor:
+    print(x)
+
+
+print("********** multiple query **********")
+multiple_query = """
+show databases;
+use bhhdeiho5kfkljyqx1ae;
+show tables;
+select * from table1;
+"""
+
+mysql_data=""
+for query in multiple_query.split(';'):
+    cursor.execute(query)
+    for x in cursor:
+        type(x)
+        print(x)
+        mysql_data.append(x)
+
+# disconnecting from server
+mydb.close() 
+
+
+button1 = st.button("show sample records")
+if button1 == True and len(mysql_data)>0:
+    st.write(mysql_data)
