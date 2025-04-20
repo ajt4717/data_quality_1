6ebe8804b869b5e07ea4a33594767ff53f417a4d
ajt4717 <ajt4717@gmail.com>
2025-04-20 08:39:55 +0530

    updated data_single with single pd.read_csv

--- pages/data_single_file.py
+++ pages/data_single_file.py
@@ -5,24 +5,23 @@ st.header("This is page1")
 
 uploaded_csv = st.file_uploader("Upload file",type=['csv'])
 
-button1 = st.button("show sample records")
-if button1 == True and uploaded_csv is not None :
+if uploaded_csv is not None:
     data = pd.read_csv(uploaded_csv)
+
+button1 = st.button("show sample records")
+if button1 == True:
     st.dataframe(data.head(4))
 
 button2 = st.button("show columns")
-if button2 == True and uploaded_csv is not None :
-    data = pd.read_csv(uploaded_csv)
+if button2 == True:
     st.write("columns : ",data.columns)
 
 button3 = st.button("show missing data")
-if button3 == True and uploaded_csv is not None :
-    data = pd.read_csv(uploaded_csv)
+if button3 == True:
     st.write("missing data : ",data.isnull())
 
 button4 = st.button("show data stats")
-if button4 == True and uploaded_csv is not None :
-    data = pd.read_csv(uploaded_csv)
+if button4 == True:
     st.write("data stats : ",data.describe()) 
 
 # if uploaded_csv is not None:
